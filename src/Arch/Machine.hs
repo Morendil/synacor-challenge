7e5c71d5302686e37f1e8dbb704262fefee8c305
Laurent Bossavit <laurent.git@bossavit.com>
2020-12-29 18:11:55 +0100

    Output

--- src/Arch/Machine.hs
+++ src/Arch/Machine.hs
@@ -1,17 +1,22 @@
 module Arch.Machine where
 import qualified Data.IntMap as I
+import Data.Maybe (fromJust)
 
 data Machine = Machine {
       halted :: Bool
     , pc :: Int
     , memory :: I.IntMap Int
+    , output :: [Int]
 }
 
 step :: Machine -> Machine
-step m = case fetch m (pc m) of
-  Just 0 -> halt $ m { pc = pc m + 1}
-  Just 21 -> noop $ m { pc = pc m + 1}
+step m = case fetch m current of
+  Just 0 -> halt $ m { pc = current + 1}
+  Just 21 -> noop $ m { pc = current + 1}
+  Just 19 -> out a (m { pc = current + 2})
   Nothing -> m
+  where current = pc m
+        a = fromJust $ fetch m $ current+1
 
 fetch :: Machine -> Int -> Maybe Int
 fetch m addr = I.lookup addr (memory m)
@@ -23,6 +28,9 @@ load list =  initial { memory = I.fromList $ indexed list }
 halt :: Machine -> Machine
 halt m = m { halted = True }
 
+out :: Int -> Machine -> Machine
+out val m = m { output = output m ++ [val] }
+
 noop :: Machine -> Machine
 noop = id
 
@@ -31,4 +39,5 @@ initial = Machine {
         halted = False
       , pc = 0
       , memory = I.empty
+      , output = []
     }
--- test/Arch/MachineSpec.hs
+++ test/Arch/MachineSpec.hs
@@ -13,6 +13,12 @@ spec = do
     it "should be halted after executing opCode 0, halt" $ do
       halted initial `shouldBe` False
       halted (halt initial) `shouldBe` True
+  describe "out" $ do
+    it "should output one character after executing opcode 19, out" $ do
+      output initial `shouldBe` []
+      output (out 65 initial) `shouldBe` [65]
+      output (step $ load [19, 65]) `shouldBe` [65]
+      pc (step $ load [19, 65]) `shouldBe` 2
   describe "step" $ do
     it "should fetch and execute an instruction" $ do
       halted (step $ load [0]) `shouldBe` True
